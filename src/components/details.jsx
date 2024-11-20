58b0808540c55e68ac9cebf74e946dda12201ab0
Tseku210y <otgonbaatartsenguun@yahoo.com>
2024-11-21 02:46:47 +0800

    add details for each engine

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,17 +1,33 @@
 import { useContext, useState } from "react";
+import { ESPDetails } from "../data/engine-detail";
 import { EPSystems } from "../data/engines";
+import { cn } from "../lib/utils";
 import { MainContext } from "../state/MainProvider";
 
 const Details = ({ next, back }) => {
   const { selectedOption } = useContext(MainContext);
-  const [itemDetail, setItemDetail] = useState(() =>
+  const [itemDetail] = useState(() =>
     EPSystems.find((item) => item.name === selectedOption),
   );
+  const [moreDetail, setMoreDetail] = useState(null);
+
+  const seeMore = () => {
+    if (!itemDetail) return;
+    if (moreDetail) {
+      setMoreDetail(null);
+      return;
+    }
+    const detail = ESPDetails[itemDetail.name];
+    if (detail) {
+      setMoreDetail(detail);
+    }
+  };
 
   return (
-    <div className="relative h-screen w-full flex flex-col gap-20 items-center justify-center">
+    <div className="relative min-h-screen w-full flex flex-col gap-20 items-center justify-center">
+      {/* Basic Details */}
       {itemDetail ? (
-        <div className="w-fit flex gap-5 text-3xl">
+        <div className={cn("w-fit flex gap-5 text-3xl", moreDetail && "mt-32")}>
           <div className="flex flex-col gap-2 items-end">
             {Object.keys(itemDetail).map((key) => (
               <span key={key} className="capitalize font-light text-white/30">
@@ -31,20 +47,52 @@ const Details = ({ next, back }) => {
       ) : (
         <div>Not found</div>
       )}
+
       {itemDetail && (
-        <button className="btn btn-link text-xl text-neutral-content">
-          see more
+        <button
+          className="btn btn-link text-xl text-neutral-content"
+          onClick={seeMore}
+        >
+          See more
         </button>
       )}
-      <button
-        onClick={back}
-        className="btn btn-outline absolute left-10 top-2/4"
-      >
+
+      {moreDetail && (
+        <div className="w-3/4 p-6 mb-32 rounded-lg shadow-lg text-white">
+          <h2 className="text-2xl font-bold mb-4">{itemDetail.name}</h2>
+          <img
+            src={moreDetail.image}
+            alt="zurag"
+            className="w-full max-h-64 object-contain mb-4"
+          />
+          <p className="mb-4 text-lg font-thin text-white/50">
+            {moreDetail.background}
+          </p>
+          <h3 className="text-xl font-semibold">References:</h3>
+          <ul className="list-disc pl-5">
+            {moreDetail.references.map((ref, index) => (
+              <li key={index}>
+                <a
+                  href={ref}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-300 underline"
+                >
+                  {ref}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {/* Navigation Buttons */}
+      <button onClick={back} className="btn btn-outline fixed left-10 top-2/4">
         Go back
       </button>
       <button
         onClick={next}
-        className="btn btn-outline btn-warning absolute right-10 top-2/4"
+        className="btn btn-outline btn-warning fixed right-10 top-2/4"
       >
         Compare
       </button>
---
+++ src/data/engine-detail.js
@@ -0,0 +1,74 @@
+export const ESPDetails = {
+  SPT100: {
+    background:
+      "The relatively recent availablity of the Russian Stationary Plasma Thruster (SPT) as a propulsion source for Western spacecraft has generated a great deal of interest in the device. The Ballistic Missile Defense Organization, BMDO, (formerly, Strategic Defense Initiative Organization) has been in the forefront of enabling the transfer of the technology to the US. In 1991, under BMDO sponsorship, a team of electric propulsion specialists from three government facilities evaluated the performance of the SPT-100 at two Russian test facilities.l That phase of the program documented the performance of the device and led the BMDO to support the acquisition of a thruster for continued life and performance testing in US laboratories, with the eventual program goal of flying SPT thrusters on the TOPAZ mission",
+    references: [
+      "https://ntrs.nasa.gov/api/citations/19940019157/downloads/19940019157.pdf",
+      "https://electricrocket.org/IEPC/IEPC-2011-329.pdf",
+    ],
+    image: "engines/sp100.jpeg",
+  },
+  "ROS 2000": {
+    background:
+      "Astrium, which currently uses the SPT-100 on its EUROSTAR 3000 busses, is developing a thruster of somewhat lower thrust/power level, designed to be used on their current buses which requires a longer lifetime and higher total impulse than the SPT-100. Supported by ESA in cooperation with the Keldysh Institute in Moscow, Inasmet in Spain, and QinetiQ in UK Astrium has developed the ROS 2000, a thruster built purely of European / Western Standard parts. ",
+    references: [
+      "https://apps.dtic.mil/sti/tr/pdf/ADA414779.pdf",
+      "https://electricrocket.org/IEPC/IEPC-2011-329.pdf",
+    ],
+    image: "engines/ros2000.jpeg",
+  },
+  HT100: {
+    background:
+      "The HT100 is a low power permanent magnet Hall thruster designed to work in the range 120W-400W. The new version HT100D is the result of the great experience gained by Alta in the last years in the field of low power Hall thrusters. Its design has been performed in order to optimize the thruster performance and to increase the lifetime. With respect to the previous version the new model presents a completely new magnetic circuit. Great attention has been devoted to the topology of magnetic field that influences thruster performance and erosion processes. In addition, with respect to the previous models, the thermal design has been improved using a passive cooling system able to reduce the thruster working temperature for a fixed power level or to increase the operating power range. Furthermore HT100D is equipped with an internal stainless steel stiffness structure able to sustain all loads, even launch loads.",
+    references: [
+      "https://electricrocket.org/IEPC/j2csts45.pdf",
+      "https://electricrocket.org/IEPC/IEPC-2011-329.pdf",
+    ],
+    image: "engines/ht100.jpeg",
+  },
+  "HEMPT 3050": {
+    background:
+      "Thales Electron Devices GmbH TEDG has started development of the High Efficiency Multistage Plasma Thruster HEMPT in the late 1990s with a first patent launched in 19981. The HEMPT represents a novel concept for ion propulsion and was initially inspired by Travelling Wave Electron Tubes TWTs. TEDG is world market leader for TWTs; development activities range back to 1970s, and so far more than 400Mh of successful TWT operation in space could be demonstrated. The first HEMPT prototype from 2001 consisted of a multi-stage collector-type ionization stage with an electron gun attached to the collector entrance2,3. The electron beam both serves to ignite the discharge in the collector and neutralizes the emitted ion beam. The collector employs different potential stages and is imbedded in a so-called Permanent Periodic Magnet PPM system which provides a linear magnetic multi cusp structure. It has been found that both the internal electron gun and the concept of determining the plasma potential by external electrodes only worked in diluted gas densities, which did not allow for sufficiently high thrust levels and thrust densities.",
+    references: [
+      "https://electricrocket.org/IEPC/IEPC-2011-236.pdf",
+      "https://electricrocket.org/IEPC/IEPC-2011-329.pdf",
+    ],
+    image: "engines/hempt.jpeg",
+  },
+  "HEMPT 30250": {
+    background:
+      "Thales Electron Devices GmbH TEDG has started development of the High Efficiency Multistage Plasma Thruster HEMPT in the late 1990s with a first patent launched in 19981. The HEMPT represents a novel concept for ion propulsion and was initially inspired by Travelling Wave Electron Tubes TWTs. TEDG is world market leader for TWTs; development activities range back to 1970s, and so far more than 400Mh of successful TWT operation in space could be demonstrated. The first HEMPT prototype from 2001 consisted of a multi-stage collector-type ionization stage with an electron gun attached to the collector entrance2,3. The electron beam both serves to ignite the discharge in the collector and neutralizes the emitted ion beam. The collector employs different potential stages and is imbedded in a so-called Permanent Periodic Magnet PPM system which provides a linear magnetic multi cusp structure. It has been found that both the internal electron gun and the concept of determining the plasma potential by external electrodes only worked in diluted gas densities, which did not allow for sufficiently high thrust levels and thrust densities.",
+    references: [
+      "https://electricrocket.org/IEPC/IEPC-2011-236.pdf",
+      "https://electricrocket.org/IEPC/IEPC-2011-329.pdf",
+    ],
+    image: "engines/hempt.jpeg",
+  },
+  "FT-150": {
+    background:
+      "THE FT-150 Field Emission Electric Propulsion (FEEP) microthruster is designed for extremely fine positioning and attitude control applications. It generates thrust by ejecting cesium ions at about 100 km/s of speed with a noise level lower than the threshold of the nano-balance used in the direct measurement,1 about 0.1 N·Hz-1/2, in the 10-2 to 10 Hz range. Ions are extracted from the emitter tip and accelerated by the electric field created between the emitter and the accelerator electrode placed in front of it. The total voltage applied to the electrodes is between 7 kV and 13 kV. The specific impulse of the FT-150 FEEP microthruster is in the typical range of ion thrusters, varying between 3000 s and 4500 s depending on operating conditions. Performance was verified by testing throughout a thrust range from 0.1 N to 150 N.",
+    references: [
+      "https://electricrocket.org/IEPC/IEPC-2011-329.pdf",
+      "https://www.academia.edu/download/43299640/Altas_FT-150_FEEP_Microthruster_Develop20160303-10565-12olod4.pdf",
+    ],
+    image: "engines/ft150.jpeg",
+  },
+  "In-FEEP": {
+    background:
+      "DURING the GOCE Microthruster Program and follow-up endurance tests 23 ARCS has collected more than 3800 hours at a mean thrust of 10 uN with one microthruster, and 18000 hours of cumulative operation with several thrusters. The lessons learned during this intensive test campaign allowed to greatly improve the performance of the indium FEEP emitters: higher mass efficiency, lower erosion, higher lifetime. These emitters have a maximum continuous thrust level of 10-12 uN. In order to fulfill the thrust requirements of missions like LISA Pathfinder (100 uN), it is necessary to cluster a certain number of emitters. In the GOCE Microthruster",
+    references: [
+      "https://arc.aiaa.org/doi/epdf/10.2514/6.2005-4385",
+      "https://electricrocket.org/IEPC/IEPC-2011-329.pdf",
+    ],
+    image: "engines/infeep.jpeg",
+  },
+  "PPS 1350": {
+    background:
+      "The qualification program included several thrusters with emphasis on one of them which followed a complete environmental and lifetime test. With an average thrust between 89 and 90 10-3 N, and a specific impulse above 1650 s on average, the thruster demonstrated a total impulse of 3.39 10° N.s under various and extreme environmental conditions. On the same cathode, the thruster demonstrate also a capability of more than 7200 cycles. We will detail the qualification phases and show the performances obtained. Designed mainly for geostationary mission, the thruster demonstrates flexible capability confirmed both by the Smart-1 ESA successful experience and by ground research tests.",
+    references: [
+      "https://arc.aiaa.org/doi/epdf/10.2514/6.2007-5197",
+      "https://electricrocket.org/IEPC/IEPC-2011-329.pdf",
+    ],
+    image: "engines/pps1350.jpeg",
+  },
+};
